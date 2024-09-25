98cc636f6eeedc4f360c087bd87fd0a2599c350a
Ali Naqvi <alibnaqvi851@gmail.com>
2024-09-24 18:08:14 -0700

    Step 2 - display counter that increments by 1 when samosa is clicked. Counter value stored in the component's state using the useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import './App.css';
+import { useState } from 'react';
 
 const App = () => {
+    const [count, setCount] = useState(0);
+
+    let multiplier = 1;
+
+    function updateCount() {
+        setCount(count + multiplier);
+    }
 
     return (
         <div className="App">
             <div className="header">
                 <h1>Samosa Selector</h1>
-                <h2>Count:</h2>
-                <img className="samosa" src="src/assets/samosa.img"/>
+                <h2>Count: {count}</h2>
+                <img src="/src/assets/samosa.img" className="samosa" onClick={updateCount} alt="Samosa" />
             </div>
-        </div> )
+        </div>
+    )
 }
 
 export default App
