94b2758db4b9093bee21c936ceb2c7cd7e7f0856
Ali Naqvi <alibnaqvi851@gmail.com>
2024-09-24 17:42:56 -0700

    Step 1

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,12 @@ const App = () => {
 
     return (
         <div className="App">
-
-        </div>
-    )
+            <div className="header">
+                <h1>Samosa Selector</h1>
+                <h2>Count:</h2>
+                <img className="samosa" src="src/assets/samosa.img"/>
+            </div>
+        </div> )
 }
 
 export default App
